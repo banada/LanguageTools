a9da553727c22bb8b4772d4204c08c9cdf9bcc4e
Nathaniel Chen <nathanielchen1@gmail.com>
2018-01-30 09:09:11 -0800

    Added number_test
    Play an audio clip for a random number.
    TODO make the number range and language configurable.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # LanguageTools
+
+## number_test.sh
+
+Requirements: `sudo apt-get install mplayer`
+
+Uses Google Translate to play an audio clip of a random number. Type the number in to see the correct answer.
+
+To change languages, replace 'ko' with another language code, e.g. 'zh' for Mandarin Chinese.
+
---
+++ number_test.sh
@@ -0,0 +1,11 @@
+NUMBER=$(( $RANDOM ));
+# For a limited range of numbers:
+# NUMBER=$(( $RANDOM%10 ));
+# NUMBER=$(( $RANDOM%1000 ));
+mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=ko&client=tw-ob&q=$NUMBER" &> /dev/null
+read NUMBER_IN;
+if [ $NUMBER_IN -eq $NUMBER ]; then
+    echo "Correct";
+else
+    echo "Wrong, the answer is: $NUMBER";
+fi;
