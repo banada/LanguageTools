4a4fd1c5e32118bd04649ebd952194dac1f3a254
Nathaniel Chen <nathanielchen1@gmail.com>
2020-02-12 17:58:21 -0800

    Added Chinese support

--- number_test.sh
+++ number_test.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+# number_test.sh
+#
+# Practice numbers in another language
+#
+# Usage:
+#   ./number_test.sh <language>
+#
+# Languages:
+#   ko: Korean
+#   zh: Chinese
+
+LANGUAGE=$1
 NUMBER=$(( $RANDOM ));
 # For a limited range of numbers:
 # NUMBER=$(( $RANDOM%10 ));
 # NUMBER=$(( $RANDOM%1000 ));
-mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=ko&client=tw-ob&q=$NUMBER" &> /dev/null
+
+echo "";
+echo "###################";
+echo "##  Number Test  ##";
+echo "###################";
+echo "";
+echo "Type the number that you hear:";
+echo "";
+
+mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=$LANGUAGE&client=tw-ob&q=$NUMBER" &> /dev/null
+
 read NUMBER_IN;
 if [ $NUMBER_IN -eq $NUMBER ]; then
     echo "Correct";
